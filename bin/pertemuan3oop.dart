ac460e62005a07ba2eb2ebcdf04a403508d4cfa1
Azka32232 <azka32232@gmail.com>
2024-03-25 07:58:25 -0700

    tugas pertemuan 3

---
+++ bin/pertemuan3oop.dart
@@ -0,0 +1,5 @@
+import 'package:pertemuan3oop/pertemuan3oop.dart' as pertemuan3oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pertemuan3oop.calculate()}!');
+}
---
+++ test/pertemuan3oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:pertemuan3oop/pertemuan3oop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
